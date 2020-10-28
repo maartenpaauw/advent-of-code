1c2a993aab66dd60f4e47c6acd622444f96c2757
Maarten Paauw <mpaauw@swis.nl>
2020-10-28 19:35:54 +0100

    feat: add scaffold command

---
+++ app/Commands/AdventScaffoldCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+
+class AdventScaffoldCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'advent:scaffold';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Scaffold the folder structure for a given year';
+
+    /**
+     * @var Carbon
+     */
+    private $carbon;
+
+    public function __construct(Carbon $carbon)
+    {
+        parent::__construct();
+
+        $this->carbon = $carbon;
+    }
+
+    public function handle(): int
+    {
+        $year = $this->ask('For what year do you want to create the scaffolding?', $this->carbon->year);
+        $days = new Collection(range(1, 25));
+
+        $days->each(function (int $day) use ($year) {
+            $name = sprintf('Year%s/Day%d/Solution', $year, $day);
+            $this->callSilent('advent:generate', ['name' => $name]);
+        });
+
+        return 0;
+    }
+}
